a3ccfc678ad71c888991b0e819147a4dd104da75
Julia Sauer <julia.sauer@hotmail.ch>
2026-06-21 14:24:44 +0200

    add files for map implementation

---
+++ android/tinySSB/app/src/main/assets/web/prod/map/map.js
@@ -0,0 +1,11 @@
+// map.js
+
+// all available operations
+const Operation = {
+    MARKER_CREATE: 'map/marker/create',
+    MARKER_UPDATE: 'map/marker/update',
+    MARKER_DELETE: 'map/marker/delete',
+    MAP_UPDATE: 'map/update',
+    PRIVACY_CHANGE: 'map/privacy/change',
+    LEAVE: 'leave'
+}
